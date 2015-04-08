24fcb922a8d1ba385df2fa8f4736d482dca74bf9
onlineTurnos <fer3zip@gmail.com>
2015-04-08 11:22:49 -0300

    Initial Commit

---
+++ TP0/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TP0
+//
+//  Created by Fernando De Caro on 8/4/15.
+//  Copyright (c) 2015 Fernando De Caro. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
