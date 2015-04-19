d1d357ec4151cdcf1b9fe6fa159f70f76dfa7482
onlineTurnos <fer3zip@gmail.com>
2015-04-18 23:11:07 -0300

    Ejercicio 1-10 Libro C

---
+++ Tp102/main.c
@@ -0,0 +1,28 @@
+/* Tp102 SSL UTN
+ * Fernando De Caro
+ * 18/4/15
+ * Eje 1-10 Libro C PG:20
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+	
+	char  c ; 
+        
+        while ( (c = getchar()) != EOF){
+      		
+		if(c == ' ')
+			c ='\\b';
+		if(c == '	')
+			c = '\\t';
+		if(c == '\')
+			c = '\\\\';               
+               putchar(c);
+		
+ 	 }
+return 0;
+
+}
+
